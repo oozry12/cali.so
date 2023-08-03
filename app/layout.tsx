6edc27a3875b82e52e2cc819ac09da14a3a97c97
wah-home-pc <wah.io@outlook.com>
2023-08-03 23:52:14 +0800

    update siteinfo

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,11 +14,11 @@ import { seo } from '~/lib/seo'
 export const metadata: Metadata = {
   metadataBase: seo.url,
   title: {
-    template: '%s | Cali Castle',
+    template: '%s | MxArt',
     default: seo.title,
   },
   description: seo.description,
-  keywords: 'Cali,Cali Castle,郭晓楠,佐玩,创始人,CEO,开发者,设计师,细节控,创新',
+  keywords: 'MxArt,创始人,艺术家,体验者,书法家,细节控,创新',
   themeColor: [
     { media: '(prefers-color-scheme: dark)', color: '#000212' },
     { media: '(prefers-color-scheme: light)', color: '#fafafa' },
@@ -38,13 +38,13 @@ export const metadata: Metadata = {
   openGraph: {
     title: {
       default: seo.title,
-      template: '%s | Cali Castle',
+      template: '%s | MxArt',
     },
     description: seo.description,
-    siteName: 'Cali Castle',
+    siteName: 'MxArt Studio',
     locale: 'zh_CN',
     type: 'website',
-    url: 'https://cali.so',
+    url: 'https://www.ko.ac.cn',
   },
   twitter: {
     site: '@thecalicastle',
